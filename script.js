623d278a853f09a6bc2b52e7b4e0de1b47a9c009
Lucas Monterazo <lsm6@cin.ufpe.br>
2022-05-19 09:33:36 -0300

    feature(main): script to change colors

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const COLOR_NAME =  document.querySelector("#description");
+const CHANGE_BUTTON = document.querySelector('#changer');
+
+const colorList = ["#F2F2F2", "#E5E5E5", "#292929"]
+
+//Index contador
+let i=0;
+
+
+function changePage() {
+    document.body.style.backgroundColor = colorList[i];
+    COLOR_NAME.innerHTML = `Background:  ` + colorList[i];
+    
+    //Atualiza contador
+    i++;
+    if(i==3) i=0;
+        
+}
+
+CHANGE_BUTTON.onclick = function () {
+    changePage();
+}
