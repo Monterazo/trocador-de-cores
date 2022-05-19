aded79f691169132640198353b763aad55601a13
Lucas Monterazo <lsm6@cin.ufpe.br>
2022-05-19 15:02:12 -0300

    feature(main): mobile and tablet breakpoints

--- main.js
+++ main.js
@@ -4,18 +4,15 @@ const CHANGE_BUTTON = document.querySelector('#changer');
 
 const colorList = ["#F2F2F2", "#C7C7C7", "#292929"]
 
-//Index contador
+//Index (contador)
 let i=0;
-
 
 function changePage() {
     document.body.style.backgroundColor = colorList[i];
     COLOR_NAME.innerHTML = `Background:  ` + colorList[i];
-    
     //Atualiza contador
     i++;
     if(i==3) i=0;
-        
 }
 
 CHANGE_BUTTON.onclick = function () {
